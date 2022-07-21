288869f38afde1e9faf7b0796c2c693537812805
BWilliams07 <104173554+BWilliams07@users.noreply.github.com>
2022-07-21 00:14:32 -0400

    "changes"

--- src/test/java/TestHelloWorld.java
+++ src/test/java/TestHelloWorld.java
@@ -1,23 +1,17 @@
-import org.junit.Test;
-
 import java.util.Scanner;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Created by LaunchCode
  */
 public class TestHelloWorld {
 
-    public class HelloWorld {
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Hello World!");
-        }
+    public static void main(String[] args) {
+        new Scanner(System.in);
+        System.out.println("Hello World!");
     }
-    @Test
-    public void testSayHello() {
-        assertEquals("Hello, World!", HelloWorld.sayHello());
-    }
-
 }
+
+
+
+
+
